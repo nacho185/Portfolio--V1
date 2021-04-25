2b646e814fef57d8c495df48a8f9852b8ca14bc8
Ignacio Aguirre <ignacio13542@gmail.com>
2021-04-25 16:43:32 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*lets*/
+let ul = document.getElementById("just-for-button-onclick");
+let navbarElements = document.querySelector(".just-for-button-onclick-class");
+let navbarHere = document.getElementById("this-navbar");
+let prevScrollpos = window.pageYOffset;
+/*lets*/
+/*navbar fadein and fade out*/
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbarHere.style.top = "";
+  } else {
+    navbarHere.style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}; /*end match media*/
+/*navbar for mobile*/ let button = document.getElementById(
+  "button-for-all-devices"
+);
+
+button.addEventListener("click", () => {
+  if (ul.style.display === "none") {
+    ul.style.display = "block";
+  } else {
+    ul.style.display = "none";
+  }
+});
+/*end navbar for mobile*/
