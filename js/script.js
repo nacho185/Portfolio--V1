e184170660e1308b8f2cb939e19a99da59746f09
Ignacio Aguirre <ignacio24687531@gmail.com>
2021-05-14 13:57:04 -0500

    🎨🎨🎨changed navbar, and links
    Changed navbar color on scroll and added link smooth transitions so its no to snappy

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
+/*navbar scroll*/
+/*change color on scroll*/
+$(function () {
+  $(document).scroll(function () {
+    let $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+/*end change color on scroll*/
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      let target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            let $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
+
 /*start loader*/
 $(window).on("load", () => {
   $(".loader-container").fadeOut("slow");
